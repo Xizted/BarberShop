2fb1bdacd8b0f4daf4e0fcf5be52567d3fd2bfc7
Osmar Ortiz <xizted79@gmail.com>
2024-06-21 21:26:08 -0400

    fixed

--- src/utils/money.ts
+++ src/utils/money.ts
@@ -1,4 +1,7 @@
 export const parseNumberToMoney = (number: number): string => {
+  if (typeof number !== 'number' || isNaN(number)) {
+    return 'N/A';
+  }
   return number.toLocaleString('en-US', {
     style: 'currency',
     currency: 'USD',
